032b017b62afbf563c84227a7dae192bb8bca106
Rostislav Kulik <45662763+r-kulik@users.noreply.github.com>
2025-03-18 22:13:59 +0300

    Documentation to the RankFusion

--- mirage/inference/rerankers/LinearCombinationReranker.py
+++ mirage/inference/rerankers/LinearCombinationReranker.py
@@ -1,4 +1,4 @@
-from typing import Optional, Self
+from typing import Dict, Optional, Self
 
 from mirage.index import QueryResult
 from .Reranker import Reranker
@@ -14,16 +14,27 @@ class LinearCombinationReranker(Reranker):
         self.vector_score_weight = vector_score_weight
 
     def __call__(self, fulltext_search_results, vector_search_results):
-        results_dict = {}
+        # -------------------------------------------
+        # results_dict contains pairs of {chunk_storage_key: relevance_score}
+        results_dict: Dict[str, float] = {}
+        # -------------------------------------------
+        # adding all vector search results to the dictionary
         for query_result in vector_search_results:
             if query_result.chunk_storage_key in results_dict:
                 raise ValueError('You are trying to add a duplicate chunk thorugh a vector search')
             results_dict[query_result.chunk_storage_key] = query_result.score * self.vector_score_weight
+        # -------------------------------------------
+        # iterating thorugh results from the fulltext search
+        # creating new entries if necessary, or adding fulltext relevance
+        # to the existing entires in the results_dict
         for query_result in fulltext_search_results:
+            
             results_dict[query_result.chunk_storage_key] = results_dict.get(
                 query_result.chunk_storage_key,
                 0
             ) + query_result.score * self.fulltext_score_weight
+        # -------------------------------------------
+        # sorting items in the dict to present them in the descending order of relevance
         return [
             QueryResult(
                 score=combined_score,
--- mirage/inference/rerankers/Reranker.py
+++ mirage/inference/rerankers/Reranker.py
@@ -11,6 +11,8 @@ from mirage.index import QueryResult
 from mirage.index.chunk_storages import ChunkStorage
 
 class Reranker(ABC):
+    """Performs a Rerunking functionality
+    """
 
     def __init__(self):
         super().__init__()
@@ -20,4 +22,18 @@ class Reranker(ABC):
         self, fulltext_search_results: List[QueryResult], 
         vector_search_results: List[QueryResult]
         ) -> List[QueryResult]:
+        """Executopn of the Rank Fusion algorithm
+
+        Parameters
+        ----------
+        fulltext_search_results : List[QueryResult]
+            Results obtained from the fulltext search
+        vector_search_results : List[QueryResult]
+            Results obtained from the vector search
+
+        Returns
+        -------
+        List[QueryResult]
+            Results ordered in the descending order according to the relevance score
+        """
         pass
